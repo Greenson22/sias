652a53600fd27c407fa9fdfd6f4b22c29f0ab94d
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-14 12:55:06 +0800

    refactor : merubah nama menjadi name dan mengembalikan nama untuk model ini

--- teacher/models.py
+++ teacher/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Teacher(models.Model):
      nuptk = models.CharField(max_length=20, unique=True)
-     nama = models.CharField(max_length=100)
+     name = models.CharField(max_length=100)
      gender = models.BooleanField(default=True)
 
      def __str__(self):
-          pass
+          return self.name
