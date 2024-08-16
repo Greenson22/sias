272d8c8256a0f0d637257a034e09a3e627090c37
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-16 20:19:24 +0800

    refactor : menambahkan ambil file dari url dan menambahkan beberapa komentar

--- sias/urls.py
+++ sias/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
@@ -12,4 +14,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
+# menambahkan url dari Teacer ViewSet
 urlpatterns += router.urls
+
+# jika di mode debug maka masukan url dari media
+if settings.DEBUG:
+     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
