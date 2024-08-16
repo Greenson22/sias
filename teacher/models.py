2c22145642d8afbe92e841cd4d16517d5f880a58
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-16 19:55:59 +0800

    refactor : menghapus komentar

--- teacher/models.py
+++ teacher/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Teacher(models.Model):
      nuptk = models.CharField(max_length=20, unique=True)
      name = models.CharField(max_length=100)
