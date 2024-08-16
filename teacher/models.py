9a1b9f339e64d8664e8377aa28e00bd9795e4c8f
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-16 20:19:46 +0800

    refactor : menambahkan attribute picture

--- teacher/models.py
+++ teacher/models.py
@@ -4,6 +4,7 @@ class Teacher(models.Model):
      nuptk = models.CharField(max_length=20, unique=True)
      name = models.CharField(max_length=100)
      gender = models.BooleanField(default=True)
+     picture = models.FileField(upload_to='picture/', blank=True)
 
      def __str__(self):
           return self.name
