d93561c3fd7fb6dbd7e9021c4aed518fb51c46fa
Greenson22 <80138308+Greenson22@users.noreply.github.com>
2024-08-16 19:53:55 +0800

    refactor : membuat TeacherViewset

--- teacher/views.py
+++ teacher/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from .models import Teacher
+from .serializer import TeacherSerializer
 
-# Create your views here.
+class TeacherViewSet(ViewSet):
+     def list(self, request):
+          queryset = Teacher.objects.all()
+          serializer = TeacherSerializer(queryset, many=True)
+          return Response(serializer.data)
